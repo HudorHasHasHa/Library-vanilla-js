4c78c44ceb248954efe2cc56b6ac7176648ecf03
Igor Jankowski <jankowskiigor5@gmail.comgit congig --global -lgit config --global user.email jankowskiigor5@gmail.com>
2021-12-05 14:03:45 +0100

    added sorting, removing, iconChanging etc..

--- js/script.js
+++ js/script.js
@@ -1,26 +1,25 @@
-// import swal from 'sweetalert';
-
 // Selectors
 const submitButton = document.getElementById("submit");
 const priority = document.getElementById("priority");
 const table = document.getElementById("tableBody");
+const deleteButtons = document.getElementsByClassName("deleteButton");
 
-let counter = 0;
 const addBook = (data) => {
-  counter++;
   let html = '';
-  table.innerHTML= '';
-  for(let row of data){
-    html += `
-      <tr class="carWrapper" id="${counter}">
-        <td valign="center" class="table-element col-3 col-sm-3" id="title">${row[0]}</td>
-        <td valign="center" class="table-element col-3 col-sm-3" id="author">${row[1]}</td>
-        <td valign="center" class="table-element col-3 col-sm-3" id="priority">${row[2]}</td>
-        <td valign="center" class="table-element col-3 col-sm-3" id="category">${row[3]}</td>
+  table.innerHTML = '';
+  for (let row of data) {
+    let id = table.children.length;
+    html = `
+      <tr class="carWrapper" id="${id}">
+        <td valign="center" class="table-element col-3 col-sm-3" id="title"><div class="table-cell">${row[1]}</div></td>
+        <td valign="center" class="table-element col-3 col-sm-3" id="author"><div class="table-cell">${row[2]}</div></td>
+        <td valign="center" class="table-element col-3 col-sm-3" id="priority"><div class="table-cell">${row[3]}</div></td>
+        <td valign="center" class="table-element col-3 col-sm-3" id="genres"><div class="last-table-cell">${row[4]}<img class="deleteButton" src="../images/delete.png"/></div></td>
       </tr> 
     `
+    table.insertAdjacentHTML('beforeend', html);
   }
-  table.insertAdjacentHTML('beforeend', html);
+  deleteElements();
 }
 
 submitButton.addEventListener('click', function (event) {
@@ -28,21 +27,21 @@ submitButton.addEventListener('click', function (event) {
   let title = this.parentNode.parentNode[0].value;
   let author = this.parentNode.parentNode[1].value;
   let priority = this.parentNode.parentNode[2].value;
-  let category = this.parentNode.parentNode[3].value;
+  let genres = this.parentNode.parentNode[3].value;
   if (
     title.length >= 1 && !/[^a-zA-Z0-9]/.test(title) &&
     author.length >= 3 && !/[^a-zA-Z]/.test(author) &&
     priority.length === 1
   ) {
     //adding row after validation
-    addRow(title,author,priority,category)
+    addRow(title, author, priority, genres);
     //clearing inputs
     document.getElementById("bookForm").reset();
   }
-  else if(!(title.length >= 1 && !/[^a-zA-Z0-9]/.test(title))){
+  else if (!(title.length >= 1 && !/[^a-zA-Z0-9]/.test(title))) {
     swal("tytuł musi składać się z przynajmniej 1 litery lub cyfer, bez znaków specjalnych")
   }
-  else if(!(author.length >= 3 && !/[^a-zA-Z]/.test(author))){
+  else if (!(author.length >= 3 && !/[^a-zA-Z]/.test(author))) {
     swal("autor musi składać się z przynajmniej 3 liter, bez znaków specjalnych i cyfer")
   }
 });
@@ -58,9 +57,11 @@ priority.addEventListener('change', function (event) {
 });
 
 // add row into the table in localStorage
-const addRow = (title,author,priority,category) => {
+const addRow = (title, author, priority, genres) => {
   let rows = get();
-  rows.push([title,author,priority,category])
+  let id = table.children.length;
+  // console.log(typeof rows);
+  rows.push([id, title, author, priority, genres]);
   save(rows)
 }
 
@@ -69,8 +70,9 @@ const get = () => {
   let rows = localStorage.getItem('table')
   if (!rows) {
     return []
+  } else {
+    return JSON.parse(rows)
   }
-  return JSON.parse(rows)
 }
 
 // saving changes
@@ -79,8 +81,122 @@ const save = data => {
   addBook(data);
 }
 
+// deleting items from table&localStorage
+const deleteElements = () => {
+  for (let i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].addEventListener('click', function () {
+      // console.log(this.parentNode.parentNode.parentNode.id);
+      // thisArray.push(this.parentNode.parentNode.parentNode.children[0].children[0].innerHTML);
+      let storageData = JSON.parse(localStorage.getItem('table'));
+      // deleting table item and item from localstorage by matching
+      for (let i = 0; i < storageData.length; i++) {
+        // console.log(this.parentNode.parentNode.parentNode.id, storageData[i][0]);
+        if (+this.parentNode.parentNode.parentNode.id === +storageData[i][0]) {
+          storageData.splice(i, 1);
+        }
+      }
+      for (let i = 0; i < storageData.length; i++) {
+        storageData[i][0] = i;
+      }
+      // console.log(storageData)
+      save(storageData);
+      this.parentNode.parentNode.parentNode.remove()
+    });
+  }
+}
+
+const switchIcon = (item, counter, allItems) => {
+  for(let i = 0; i<allItems.length; i++){
+    allItems[i].children[0].children[0].classList.remove("fa-arrow-up");
+  }
+  if(counter%2!=0){
+    item.children[0].children[0].classList.add("fa-arrow-up");
+  }else{
+    item.children[0].children[0].classList.remove("fa-arrow-up");
+  }
+}
+
+// sorting by every header of the table with vanilajs
+const sorting = () => {
+  const title = document.getElementById("header-title");
+  const author = document.getElementById("header-author");
+  const priority = document.getElementById("header-priority");
+  const genres = document.getElementById("header-genres");
+  const sortDefault = document.getElementById("sortDefault");
+  //adding counters to check when user want to sort asc and when decs
+  let titleCount=2, authorCount=2, priorityCount=2, genresCount=2;
+  let arr = [];
+  let countersArr = [];
+  // variable for switchicon reseting the icons
+  let allItems = [];
+  allItems.push(title,author,priority,genres);
+  countersArr.push(titleCount,authorCount,genresCount);
+  arr.push(title,author,genres);
+  const defaultTable = get();
+  // console.log(arr)
+  for(let i = 0; i<arr.length; i++){
+    // console.log(arr);
+    arr[i].addEventListener('click', function () {
+      const data = get();
+      if(countersArr[i]%2==0){
+        data.sort((a,b) => {
+          if (a[i+1] === b[i+1]){
+            return 0;
+          }
+          return (a[i+1] < b[i+1]) ? -1 : 1;
+        });
+        ++countersArr[i];
+      }
+      else{
+        data.sort((a,b) => {
+          if (b[i+1] === a[i+1]){
+            return 0;
+          }
+          return (b[i+1] < a[i+1]) ? -1 : 1;
+        });
+        ++countersArr[i];
+      }
+      // console.log(data);
+      save(data)
+      switchIcon(this, countersArr[i], allItems);
+    });
+  }
+  priority.addEventListener('click', function(){
+    const data = get();
+    if(priorityCount%2==0){
+      data.sort((a,b) => {
+        if (a[3] === b[3]){
+            return 0;
+        }
+        return (a[3] > b[3]) ? -1 : 1;
+      });
+      ++priorityCount;
+    }
+    else{
+      data.sort((a,b) => {
+        if (b[3] === a[3]){
+            return 0;
+        }
+        return (b[3] > a[3]) ? -1 : 1;
+      });
+      ++priorityCount;
+    }
+    // console.log(data);
+    save(data);
+    switchIcon(this, priorityCount, allItems);
+  });
+  sortDefault.addEventListener('click', function(event){
+    event.preventDefault();
+    save(defaultTable);
+    switchIcon(this, priorityCount, allItems);
+  });
+}
+
+//init
 const appInit = () => {
   addBook(get());
+  deleteElements();
+  sorting();
 }
 
 appInit();
